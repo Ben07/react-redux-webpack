b20b7771391cea0c675371b673423f2b230abcaf
ben <zhongwangjun@hz-health.cn>
2016-11-18 16:21:48 +0800

    hide devtools default and add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0,
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+    },
+    "globals": {
+        "UM": true,
+        "XLSX":true
+    }
+};
